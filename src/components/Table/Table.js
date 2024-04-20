1876f8b04119b8f48a11f512afe230cb3bf8af02
Gaganpreet Singh <dmgaganpreet@gmail.com>
2024-04-20 19:40:00 +0530

    Added <Modal> in <Table> and condionally rended based on edit.

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Table.css";
 import axios from "axios";
+import Modal from "../Modal/Modal";
 
 const Table = ({ headings, rows }) => {
   const API_endpoint = " https://jsonplaceholder.typicode.com";
+  const [modalOpen, setModalOpen] = useState(false);
+  const [userData, setUserData] = useState({});
+
+  const openEditModal = (rowData) => {
+    setUserData(rowData);
+    setModalOpen(true);
+  };
 
   const deleteUser = async (userId) => {
     try {
@@ -17,33 +25,36 @@ const Table = ({ headings, rows }) => {
   };
 
   return (
-    <table className="table">
-      <thead>
-        {headings.map((heading) => {
-          return <th>{heading}</th>;
-        })}
-      </thead>
+    <>
+      <table className="table">
+        <thead>
+          {headings.map((heading) => {
+            return <th>{heading}</th>;
+          })}
+        </thead>
 
-      {rows.map((row) => {
-        return (
-          <tr>
-            <td>{row.id}</td>
-            <td>{row.firstname}</td>
-            <td>{row.lastname}</td>
-            <td>{row.email}</td>
-            <td>{row.department}</td>
-            <td>Edit</td>
-            <td
-              onClick={() => {
-                deleteUser(row.id);
-              }}
-            >
-              Delete
-            </td>
-          </tr>
-        );
-      })}
-    </table>
+        {rows.map((row) => {
+          return (
+            <tr>
+              <td>{row.id}</td>
+              <td>{row.firstname}</td>
+              <td>{row.lastname}</td>
+              <td>{row.email}</td>
+              <td>{row.department}</td>
+              <td onClick={() => openEditModal(row)}>Edit</td>
+              <td
+                onClick={() => {
+                  deleteUser(row.id);
+                }}
+              >
+                Delete
+              </td>
+            </tr>
+          );
+        })}
+      </table>
+      {modalOpen && <Modal data={userData}></Modal>}
+    </>
   );
 };
 
